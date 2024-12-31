b08310ea7b73c9619a8657df02a854d8575dcbef
Dan <dantrue25@gmail.com>
2024-12-31 16:51:26 -0500

    Initial Commit

---
+++ RayTracing/main.cpp
@@ -0,0 +1,14 @@
+//
+//  main.cpp
+//  RayTracing
+//
+//  Created by Dan True on 12/31/24.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
