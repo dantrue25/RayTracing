ca809cb89ad16ea427c7ad793053690f893fbdaa
Dan <dantrue25@gmail.com>
2025-01-02 21:36:08 -0500

    Draw colored rectangle 16:9

--- RayTracing/main.cpp
+++ RayTracing/main.cpp
@@ -1,27 +1,34 @@
+#include "color.h"
+#include "vec3.h"
+
 #include <iostream>
 
 int main() {
 
     // Image
 
-    int image_width = 256;
-    int image_height = 256;
+    auto aspect_ratio = 16.0 / 9.0;
+    int image_width = 400;
+
+    // Calculate the image height, and ensure that it's at least 1.
+    int image_height = int(image_width / aspect_ratio);
+    image_height = (image_height < 1) ? 1 : image_height;
+
+    // Viewport widths less than one are ok since they are real valued.
+    auto viewport_height = 2.0;
+    auto viewport_width = viewport_height * (double(image_width)/image_height);
 
     // Render
 
     std::cout << "P3\n" << image_width << ' ' << image_height << "\n255\n";
 
     for (int j = 0; j < image_height; j++) {
+        std::clog << "\rScanlines remaining: " << (image_height - j) << ' ' << std::flush;
         for (int i = 0; i < image_width; i++) {
-            auto r = double(i) / (image_width-1);
-            auto g = double(j) / (image_height-1);
-            auto b = 0.0;
-
-            int ir = int(255.999 * r);
-            int ig = int(255.999 * g);
-            int ib = int(255.999 * b);
-
-            std::cout << ir << ' ' << ig << ' ' << ib << '\n';
+            auto pixel_color = color(double(i)/(image_width-1), double(j)/(image_height-1), 0);
+            write_color(std::cout, pixel_color);
         }
     }
+
+    std::clog << "\rDone.                 \n";
 }
